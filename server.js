65690c82250a3e4f955bc8492262d3462c01900d
ionox0 <ionox0@gmail.com>
2016-02-09 03:14:45 +0000

    asdf

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ io.on('connection', function(socket){
   console.log('a user connected');
 
   socket.on('chat message', function(msg){
-    fs.appendFile('/tmp/chat', msg);
+    fs.appendFile('/tmp/chat', msg + '\n');
     io.emit('chat message', msg);
   });
 
